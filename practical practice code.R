8d1133ebb25cf927e0199e62bf084829257f2d6d
Syreeta Shigematsu <47398358+SShigematsu4@users.noreply.github.com>
2019-04-30 21:47:39 -0500

    practical practice coding

--- practical practice code.R
+++ practical practice code.R
@@ -6,29 +6,58 @@
 
 #data saved as: sample-data.csv
 
-
 library(tidyverse)
 
-#6 tibble - species A & B + height
+
+#all data (excluding NAs)
 sampledata <- read_csv("sample-data.csv") %>%
-  select(species, height) %>%
-  filter(species != "C")
+  filter(complete.cases(.))
 
 sampledata
 
-#tibble excluding NAs
-data <- sampledata %>%
-  filter(complete.cases(.))
+#6 tibble - species A & B + height (excluding NAs)
 
-data
+data1 <- sampledata %>%
+  select(species, height) %>%
+  filter(species != "C")
 
-#tibble w/ mean & SE
-data %>%
+print(data1)
+
+#tibble w/ mean & SE for species A & B
+data1 %>%
   group_by(species) %>%
   summarize(mean = mean(height, na.rm = TRUE),
             standar = sd(height, na.rm = TRUE)
             )
 
+#(for 7) tibble w/ all species + height (excluding NAs)
+data2 <- sampledata %>%
+  select(species, height)
+  
+print(data2)
+
 #7 histogram of heights w/ facets for species x3
-ggplot(sampledata) +
-  geom_histogram(aes(height))
+data2 %>%
+  group_by(species) %>%
+  ggplot(aes(height)) +
+  geom_histogram(binwidth = 2, color="purple") +
+  facet_grid(~ species) +
+  labs(x = "Species", y = "Height")
+
+#(for 8) tibble w/ all species + height & weight (excluding NAs)
+data3 <- sampledata %>%
+  select(species, height, weight)
+
+print(data3)
+
+#8 scatterplot of heights & weights  
+  
+#(for 8) tibble w/ all species + height & weight
+Tdata3 <- data3 %>%
+  group_by(species) %>%
+  ggplot(aes(height)) +
+  geom_point(na.rm = TRUE, aes(height, weight, colour = species))
+
+print(Tdata3 + ggtitle("Species A, B and C: Height and Weight")) +
+  labs(y="Weight", x = "Height")
+  
